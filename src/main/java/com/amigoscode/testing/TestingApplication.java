cdec93a45876c28d4eda43d9c4a1e2d83ae3badf
KonarzewskiP <pawelkonarzewski13@gmail.com>
2023-02-17 20:41:55 +0000

    testing git commit

--- src/main/java/com/amigoscode/testing/TestingApplication.java
+++ src/main/java/com/amigoscode/testing/TestingApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestingApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TestingApplication.class, args);
 	}
