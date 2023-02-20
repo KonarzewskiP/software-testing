2feb3d9d73f855fd383a95bf254d4f41952e0694
Pawel <pawel@tracmobilty.co.uk>
2023-02-20 12:28:33 +0000

    Add latest Google GSON library because of the following error from Stripe API:  - 'java.lang.NoClassDefFoundError: com/google/gson/Reflect AccessFilter' Add test to Stripe Service

--- src/test/java/com/amigoscode/testing/payment/stripe/StripeServiceTest.java
+++ src/test/java/com/amigoscode/testing/payment/stripe/StripeServiceTest.java
@@ -1,11 +1,26 @@
 package com.amigoscode.testing.payment.stripe;
 
+import com.amigoscode.testing.payment.CardPaymentCharge;
 import com.amigoscode.testing.payment.Currency;
+import com.stripe.exception.ApiConnectionException;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Charge;
+import com.stripe.net.RequestOptions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import java.math.BigDecimal;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyMap;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 
 class StripeServiceTest {
 
@@ -16,24 +31,64 @@ class StripeServiceTest {
 
     @BeforeEach
     void setUp() {
+        MockitoAnnotations.initMocks(this);
         underTest = new StripeService(stripeApi);
     }
 
     @Test
-    void isShouldChargeCard() {
+    void isShouldChargeCard() throws StripeException {
         //Given
         String cardSource = "0x0x0x";
         BigDecimal amount = new BigDecimal("10.00");
-        Currency usd = Currency.USD;
-        String zakat = "Zakat";
+        Currency currency = Currency.USD;
+        String description = "Zakat";
+
+        //... Charge has been successful
+        Charge charge = new Charge();
+        charge.setPaid(true);
+        given(stripeApi.create(anyMap(),any())).willReturn(charge);
 
         //When
-        underTest.chargeCard(
-                cardSource,
-                amount,
-                usd,
-                zakat);
+        CardPaymentCharge cardPaymentCharge = underTest.chargeCard(cardSource, amount, currency, description);
 
         //Then
+        // ... Stripe API was called with correct input parameters
+        ArgumentCaptor<Map<String, Object>> mapArgumentCaptor = ArgumentCaptor.forClass(Map.class);
+        ArgumentCaptor<RequestOptions> optionsArgumentCaptor = ArgumentCaptor.forClass(RequestOptions.class);
+
+        then(stripeApi).should().create(mapArgumentCaptor.capture(), optionsArgumentCaptor.capture());
+
+        Map<String, Object> requestMap = mapArgumentCaptor.getValue();
+
+        assertThat(requestMap.keySet()).hasSize(4);
+        // ... correct parameters were passed to the Stripe API
+        assertThat(requestMap.get("amount")).isEqualTo(amount);
+        assertThat(requestMap.get("currency")).isEqualTo(currency);
+        assertThat(requestMap.get("source")).isEqualTo(cardSource);
+        assertThat(requestMap.get("description")).isEqualTo(description);
+
+        RequestOptions options = optionsArgumentCaptor.getValue();
+        assertThat(options).isNotNull();
+
+        // ... the charge was successful
+        assertThat(cardPaymentCharge.isCardDebited()).isTrue();
+    }
+
+    @Test
+    void isShouldThrowExceptionWhenChargeCard() throws StripeException {
+        //Given
+        String cardSource = "0x0x0x";
+        BigDecimal amount = new BigDecimal("10.00");
+        Currency currency = Currency.USD;
+        String description = "Zakat";
+
+        //... Charge has been unsuccessful
+        given(stripeApi.create(anyMap(),any())).willThrow(new ApiConnectionException(""));
+
+        //When
+        //Then
+        assertThatThrownBy(() -> underTest.chargeCard(cardSource,amount,currency,description))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("Cannot make stripe charge");
     }
 }
